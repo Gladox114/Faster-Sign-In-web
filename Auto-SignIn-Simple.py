0f06542354401d02931c582c2fe42ac9114a93e4
Fuyu <fuyu@pop-os.localdomain>
2020-06-15 14:05:31 +0200

    detect end page

--- Auto-SignIn-Simple.py
+++ Auto-SignIn-Simple.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
 # config #
-username = "yourusername"
-passcode = "yourpasscode"
+username = "username"
+passcode = "passcode"
 
-browser = "phantomjs" # phantomjs,firefox,chrome
+browser = "firefox" # phantomjs,firefox,chrome
+#url = "http://google.com"
 url = "http://172.18.4.1"
-delay = 5 # delay till the browser closes after sign in. The server needs to process your sign in
+#delay = 5 # delay till the browser closes after sign in. The server needs to process your sign in
 ##########
 
 # import #
 from selenium import webdriver
-import times
+import time
 ##########
 
 #set your web driver
@@ -36,7 +37,14 @@ driver.find_element_by_name("visitor_accept_terms").click()
 
 #Find the login button
 test=driver.find_element_by_xpath("//input[@value='Log In']").click()
-
-
+"""
+# Cheaply closing the browser
 time.sleep(delay)
 driver.quit()
+"""
+while True:
+	if driver.current_url.find("google"):
+		print("pass cuz of",driver.current_url)
+		driver.quit()
+		break
+	time.sleep(0.1)
